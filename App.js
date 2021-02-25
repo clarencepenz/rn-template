1d2b853a6d2316a570fe7460af1181e4fcd757bb
clarencepenz <clarencepenz@users.noreply.github.com>
2021-02-25 17:47:31 +0100

    initial commit

---
+++ App.js
@@ -0,0 +1,29 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  View,
+  Text,
+} from 'react-native';
+
+
+const App = () => {
+  return (
+    <>
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <Text>
+          First Edit 🎉 😄💞
+        </Text>
+    </View>
+    </>
+  );
+};
+
+
+export default App;
