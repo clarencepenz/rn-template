c5fb08fd9bd3b0093d8385f944478fd2fffb42d0
clarencepenz <clarencepenz@users.noreply.github.com>
2021-03-03 00:16:58 +0100

    finished onboarding screen

--- app/screens/OnBoarding/OnBoarding.js
+++ app/screens/OnBoarding/OnBoarding.js
@@ -15,6 +15,21 @@ import { theme, data } from '../../constants'
 const { COLORS, FONTS, SIZES } = theme;
 
 const OnBoarding = ({navigation}) => {
+
+  const [ completed, setCompleted ] = React.useState(false)
+
+  const scrollX = new Animated.Value(0)
+
+  React.useEffect(() => {
+      scrollX.addListener(({ value }) => {
+        if(Math.floor(value / SIZES.width) === data.length - 1) {
+          setCompleted(true)
+        }
+      })
+
+      return () => scrollX.removeListener();
+  }, [])
+
   function renderContent() {
     return(
       <Animated.ScrollView
@@ -22,9 +37,13 @@ const OnBoarding = ({navigation}) => {
           pagingEnabled
           scrollEnabled
           decelerationRate={0}
-          scrollEventThrottle={16}
+          scrollEventThrottle={13}
           snapToAlignment='center'
           showsHorizontalScrollIndicator={false}
+          onScroll={Animated.event([
+            {nativeEvent: {contentOffset: { x: scrollX } } },
+
+          ], { useNativeDriver: false })}
       >
           {data.map((item, index) => (
             <View
@@ -49,7 +68,7 @@ const OnBoarding = ({navigation}) => {
                }}
               >
                 <Text style={{
-                    ...FONTS.h1,
+                    ...FONTS.h2,
                     color: COLORS.grey,
                     textAlign: 'center',
                   }}
@@ -57,7 +76,7 @@ const OnBoarding = ({navigation}) => {
                   {item.title}
                 </Text>
                 <Text style={{
-                    ...FONTS.body3,
+                    ...FONTS.body4,
                     color: COLORS.grey,
                     textAlign: 'center',
                   }}
@@ -65,6 +84,25 @@ const OnBoarding = ({navigation}) => {
                   {item.description}
                 </Text>
               </View>
+
+              <TouchableOpacity
+                  style={{
+                    backgroundColor: COLORS.blue,
+                    color: COLORS.white,
+                    position: 'absolute',
+                    bottom: 10,
+                    left: 0,
+                    width: 100,
+                    justifyContent: 'center',
+                    height: 50,
+                    paddingLeft: 15,
+                    borderTopRightRadius: 30,
+                    borderBottomRightRadius: 30
+                  }}
+                  onPress={() => navigation.navigate('NewPage')}
+              >
+                <Text style={{ ...FONTS.h3, color: COLORS.white}}>{ completed ? "Lets's go" : "Skip"}</Text>
+              </TouchableOpacity>
               {/* <Text style={{color: COLORS.black, textAlign: 'center', padding: SIZES.padding}}>{item.title}</Text> */}
             </View>
           ))}
@@ -74,14 +112,35 @@ const OnBoarding = ({navigation}) => {
 
   
   function dots(){
+
+    const dotPosition = Animated.divide(scrollX, SIZES.width)
+
     return (
       <View style={styles.dotContainer}>
-        {data.map((item, index) => (
-            <View
-            key={index}
-            style={[styles.dots, { width: 10, height: 10 }]}
-          ></View>
-        ))}
+        {data.map((item, index) => {
+
+          const opacity  = dotPosition.interpolate({
+             inputRange: [index - 1, index, index + 1],
+             outputRange: [0.3, 1, 0.3],
+             extrapolate: "clamp"
+          })
+
+          const dotSize  = dotPosition.interpolate({
+            inputRange: [index - 1, index, index + 1],
+            outputRange: [0, 10, 0],
+            extrapolate: "clamp"
+         })
+
+         return (
+           <Animated.View
+              key={`dot-${index}`}
+              opacity={opacity}
+              style={[styles.dots, { width: dotSize, height: dotSize }]}
+           >
+
+            </Animated.View>
+          )
+         })}
 
       </View>
     )
@@ -128,12 +187,16 @@ const styles = StyleSheet.create({
   },
   dotContainer: {
     flexDirection: 'row',
-    height: SIZES.padding + 8,
+    height: SIZES.padding,
   },
   dots: {
     borderRadius: SIZES.radius,
+    paddingTop: 3,
+    paddingBottom: 3,
+    paddingLeft: 7,
+    paddingRight: 7,
     backgroundColor: COLORS.blue,
-    marginHorizontal: SIZES.radius / 2
+    marginHorizontal: SIZES.radius - 8
   }
 
 })
